9587e7effcb63768055a857a399573ba767e57d6
JongUk Jeong <jongukjeong@gmail.com>
2020-07-21 12:46:08 +0900

    first upoad
    first upoad

---
+++ 연습문제/practice01/Problem01.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Problem01 {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for(i=1;   ; i++){ 
+			if( ){
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
